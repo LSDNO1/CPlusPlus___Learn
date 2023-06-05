dc2466c50e53cfec4bb7ba9f35361d099d8d3d2c
LSD <liushaodong11@gmail.com>
2023-06-05 21:01:38 +0800

    static_variable_first change

---
+++ Singleton.cpp
@@ -0,0 +1,5 @@
+//
+// Created by WLSD on 2023/6/5.
+//
+
+#include "Singleton.h"
---
+++ Singleton.h
@@ -0,0 +1,14 @@
+//
+// Created by WLSD on 2023/6/5.
+//
+
+#ifndef C___TEST_SINGLETON_H
+#define C___TEST_SINGLETON_H
+
+
+class Singleton {
+
+};
+
+
+#endif //C___TEST_SINGLETON_H
